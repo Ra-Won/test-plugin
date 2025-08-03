b36e94170ad6c9221ef3be5d2491ef86c3dd949d
Prajyot Ramteke <78234306+Ra-Won@users.noreply.github.com>
2025-08-03 22:20:52 +0530

    Update Badge.tsx

--- packages/components/src/Badge/Badge.tsx
+++ packages/components/src/Badge/Badge.tsx
@@ -1,4 +1,5 @@
-import React, { ButtonHTMLAttributes, ReactNode } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Badge.module.css';
 
 export interface BadgeProps extends ButtonHTMLAttributes<HTMLButtonElement> {
